199f7d7c74144e0dfabcaf00f9b9fa529eb74ef5
junyong <chal728@naver.com>
2025-03-13 14:28:58 +0900

    sadf

--- gatsby-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  pathPrefix: `/wedding_invitation`,
-  
-  siteMetadata: {
-    title: `weddingInvitation`,
-    siteUrl: `https://www.yourdomain.tld`
-  },
-  plugins: ["gatsby-plugin-styled-components"]
-};
